3115037f4e2ed8e278be4018404ab746b14c85ad
Xiao Ley <xiao.ley@outlook.com>
2025-05-20 18:19:07 +0800

    refactor(api): 调整登录接口

--- app/http/api/auth.py
+++ app/http/api/auth.py
@@ -16,9 +16,9 @@ from app.exceptions.exception import (
     InvalidEmailError,
 )
 from app.http import deps
-from app.schemas.auth import TokenSc, TokenStatusSc
 from app.schemas.common import BoolSc
 from app.schemas.oauth2 import OAuth2CellphoneSc, OAuth2PasswordSc
+from app.schemas.token import TokenSc, TokenStatusSc
 from app.schemas.user import UserCreateRecvSc
 from app.services.auth import random_code_verifier
 from app.services.auth.grant import (
@@ -38,20 +38,9 @@ router = APIRouter(
 )
 
 
-@router.post('/token/form', response_model=TokenSc,
-             name='用户名+密码登录（表单模式）')
-async def token_form(request_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-                     client_ip: str = Depends(deps.get_request_ip),
-                     session: AsyncSession = Depends(deps.get_db)):
-    grant = PasswordGrant(session, client_ip, request_data)
-    token_data = await grant.respond()
-    await session.commit()
-    return token_data
-
-
-@router.post('/token', response_model=TokenSc,
+@router.post('/login', response_model=TokenSc,
              name='用户名+密码登录（JSON模式）')
-async def token(request_data: OAuth2PasswordSc,
+async def login(request_data: OAuth2PasswordSc,
                 client_ip: str = Depends(deps.get_request_ip),
                 session: AsyncSession = Depends(deps.get_db)):
     grant = PasswordGrant(session, client_ip, request_data)
@@ -60,15 +49,47 @@ async def token(request_data: OAuth2PasswordSc,
     return token_data
 
 
-@router.post('/token/admin', response_model=TokenSc,
+@router.post('/login/form', response_model=TokenSc,
+             name='用户名+密码登录（表单模式）')
+async def form_login(request_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                     client_ip: str = Depends(deps.get_request_ip),
+                     session: AsyncSession = Depends(deps.get_db)):
+    grant = PasswordGrant(session, client_ip, request_data)
+    token_data = await grant.respond()
+    await session.commit()
+    return token_data
+
+
+@router.post('/login/cellphone', response_model=TokenSc,
+             name='手机号+验证码登录')
+async def cellphone_login(request_data: OAuth2CellphoneSc,
+                          client_ip: str = Depends(deps.get_request_ip),
+                          session: AsyncSession = Depends(deps.get_db)):
+    grant = CellphoneGrant(session, client_ip, request_data)
+    token_data = await grant.respond()
+    await session.commit()
+    return token_data
+
+
+@router.post('/login/admin', response_model=TokenSc,
              name='管理员登录')
-async def token_admin(request_data: OAuth2PasswordSc,
+async def admin_login(request_data: OAuth2PasswordSc,
                       client_ip: str = Depends(deps.get_request_ip),
                       session: AsyncSession = Depends(deps.get_db)):
     grant = PasswordGrant(session, client_ip, request_data)
     token_data = await grant.respond(is_admin=True)
     await session.commit()
     return token_data
+
+
+@router.post('/logout', response_model=BoolSc,
+             name='退出登录')
+async def logout(token: str = Depends(deps.oauth2_token),
+                 client_ip: str = Depends(deps.get_request_ip),
+                 session: AsyncSession = Depends(deps.get_db)):
+    await cancel_grant(session=session, client_ip=client_ip, token=token)
+    await session.commit()
+    return BoolSc(success=True)
 
 
 @router.post('/signup', response_model=BoolSc,
@@ -87,17 +108,7 @@ async def signup(user_create: UserCreateRecvSc,
     return BoolSc(success=True)
 
 
-@router.post('/logout', response_model=BoolSc,
-             name='退出登录')
-async def logout(token: str = Depends(deps.oauth2_token),
-                 client_ip: str = Depends(deps.get_request_ip),
-                 session: AsyncSession = Depends(deps.get_db)):
-    await cancel_grant(session=session, client_ip=client_ip, token=token)
-    await session.commit()
-    return BoolSc(success=True)
-
-
-@router.post('/email/verification_code', response_model=BoolSc,
+@router.post('/verification_code/email', response_model=BoolSc,
              name='发送邮箱验证码', description="发送邮箱验证码，验证码在10分钟内有效")
 async def send_email_verification_code(email: str = Body(..., embed=True, description="邮箱地址")):
     if not is_valid_email(email, True):
@@ -108,24 +119,13 @@ async def send_email_verification_code(email: str = Body(..., embed=True, descri
     return BoolSc(success=True)
 
 
-@router.post('/cellphone/token', response_model=TokenSc,
-             name='手机号+验证码登录')
-async def cellphone_token(request_data: OAuth2CellphoneSc,
-                          client_ip: str = Depends(deps.get_request_ip),
-                          session: AsyncSession = Depends(deps.get_db)):
-    grant = CellphoneGrant(session, client_ip, request_data)
-    token_data = await grant.respond()
-    await session.commit()
-    return token_data
-
-
-@router.post('/cellphone/verification_code', response_model=BoolSc,
-             name='发送手机验证码')
+@router.post('/verification_code/cellphone', response_model=BoolSc,
+             name='发送手机验证码，验证码在5分钟内有效')
 async def send_cellphone_verification_code(cellphone: str = Body(..., embed=True, description="手机号码")):
     if not is_chinese_cellphone(cellphone):
         raise InvalidCellphoneError()
 
-    code = await random_code_verifier.make(cellphone)
+    code = await random_code_verifier.make(cellphone, 60 * 5)
     await sms_sender.send_verification_code(cellphone, code)
     return BoolSc(success=True)
 
