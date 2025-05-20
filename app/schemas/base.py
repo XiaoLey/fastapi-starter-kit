9e09bb4fe6c03fb3b40143af6ab301f0ebcd4f0c
Xiao Ley <xiao.ley@outlook.com>
2025-05-21 00:17:13 +0800

    feat(schemas): 新增JWT数据结构

--- app/schemas/base.py
+++ app/schemas/base.py
@@ -9,3 +9,12 @@ class BaseSc(BaseModel):
         from_attributes=True,       # 允许使用类属性填充（而不是只能用字典填充，旧名字叫做 orm_mode）
         populate_by_name=True,       # 允许通过字段名来填充（而不是只能通过别名来填充）
     )
+
+
+class BaseWithExtrasSc(BaseModel):
+    model_config = ConfigDict(
+        extra="allow",              # 允许未知字段
+        validate_assignment=True,
+        from_attributes=True,
+        populate_by_name=True,
+    )
