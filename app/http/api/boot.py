945ebc69c82d57d7b8f7653cabbbf5a73ffb6fd2
Xiao Ley <xiao.ley@outlook.com>
2025-05-20 18:20:00 +0800

    refactor(api): 移除多余通断检测接口

--- app/http/api/boot.py
+++ app/http/api/boot.py
@@ -1,15 +1,8 @@
 from fastapi import APIRouter
 
-from app.schemas.common import BoolSc
-
 router = APIRouter()
 
 
 @router.get('/ping')
 def ping():
     return "pong"
-
-
-@router.get('/health', name='检查服务是否可用')
-async def health():
-    return BoolSc(success=True)
