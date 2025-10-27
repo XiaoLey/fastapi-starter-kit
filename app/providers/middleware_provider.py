89161685334051ffba8c8187ec52dddbcbca1917
Xiao Ley <xiao.ley@outlook.com>
2025-10-27 16:57:44 +0800

    refactor(middleware_provider): 调整模块路径分隔符，统一风格

--- app/providers/middleware_provider.py
+++ app/providers/middleware_provider.py
@@ -4,4 +4,4 @@ from app.support.modules_helper import execute_function_in_all_modules
 
 
 def register(app: FastAPI):
-    execute_function_in_all_modules('app/http/middleware', 'register', app)
+    execute_function_in_all_modules('app.http.middleware', 'register', app)
